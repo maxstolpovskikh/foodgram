91c516e968f75f8c6ebdcbc715413372f0a19e27
Max Stolpovskikh <maximstolpovskikh@gmail.com>
2024-11-12 00:46:39 +0700

    start apps

---
+++ backend/foodgram_backend/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.username
