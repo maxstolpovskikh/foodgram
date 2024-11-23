1ef6f039f36d69f5d33a005c9434408d49bc0709
Max Stolpovskikh <maximstolpovskikh@gmail.com>
2024-11-24 02:49:13 +0700

    fix setting.py csrf

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '*')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
